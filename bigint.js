11859cf5f5613b710a6d09a05739930c550b0e86
DamonAnderson <Damon.N.Anderson@gmail.com>
2014-01-09 22:59:05 -0500

    Update bigint.js
    Better handling with negative integers. Expanded increment and added decrement.

--- bigint.js
+++ bigint.js
@@ -8,7 +8,16 @@ var BigInt = function() {
 	// Set bigint to an arbitrary string of digits
 	this.set = function(s) {
 		digits = [];
-		for (var i=s.length-1;i>=0;i--) {
+		var start;
+		if (s[0] == '-') {
+			start = 1;
+			negative = true;
+		}
+		else {
+			start = 0;
+			negative = false;
+		}
+		for (var i=s.length-1;i>=start;i--) {
 			digits.push(parseInt(s[i]));
 		}
 		return this;
@@ -16,13 +25,13 @@ var BigInt = function() {
 	// Convert this bigint to its digit-string
 	this.str = function() {
 		var s = '';
+		if (negative) { s += '-'; }
 		for (var i=digits.length-1;i>=0;i--) {
 			s += digits[i];
 		}
 		return s;
 	};
-	// Increment this bigint
-	this.inc = function() {
+	function _inc() {
 		var i = 0;
 		digits[i]++;
 		while (digits[i] > 9 && i < digits.length-1) {
@@ -34,6 +43,31 @@ var BigInt = function() {
 			digits[i-1] -= 10;
 			digits.push(1);
 		}
+	}
+	function _dec() {
+		var i = 0;
+		digits[i]--;
+		while (digits[i] < 0 && i < digits.length-1) {
+			digits[i] = 9;
+			i++;
+			digits[i]--;
+		}
+		if (digits[digits.length-1] == 0 && digits.length > 1) {
+			digits.pop(digits.length-1);
+		}
+	}
+	// Increment this bigint
+	this.inc = function() {
+		if (negative) {
+			_dec();
+		} else {
+			_inc();
+		}
+		return this;
+	};
+	// Decrement this bigint
+	this.dec = function() {
+		_dec();
 		return this;
 	};
 };
