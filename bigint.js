5f8b5263a01f311b1d5a97b5bf0f6c232c3666a0
DamonAnderson <Damon.N.Anderson@gmail.com>
2014-01-10 23:56:42 -0500

    Added comparison functions
    Added logical comparison functions - less than, greater than, equality.

--- bigint.js
+++ bigint.js
@@ -2,63 +2,157 @@
 // A Big Integer implementation in Javascript
 
 var BigInt = function() {
-	var digits = [0],		// Store digits least-significant first
-		negative = false;	// Flag for negativity
+
+	this.digits = [0];		// Store digits least-significant first
+	this.negative = false;	// Flag for negativity
 	
 	// Set bigint to an arbitrary string of digits
 	this.set = function(s) {
-		digits = [];
+		this.digits = [];
 		var start;
 		if (s[0] == '-') {
 			start = 1;
-			negative = true;
+			this.negative = true;
 		}
 		else {
 			start = 0;
-			negative = false;
+			this.negative = false;
 		}
 		for (var i=s.length-1;i>=start;i--) {
-			digits.push(parseInt(s[i]));
+			this.digits.push(parseInt(s[i]));
 		}
 		return this;
 	};
 	// Convert this bigint to its digit-string
 	this.str = function() {
 		var s = '';
-		if (negative) { s += '-'; }
-		for (var i=digits.length-1;i>=0;i--) {
-			s += digits[i];
+		if (this.negative) { s += '-'; }
+		for (var i=this.digits.length-1;i>=0;i--) {
+			s += this.digits[i];
 		}
 		return s;
 	};
-	function _inc() {
-		var i = 0;
-		digits[i]++;
-		while (digits[i] > 9 && i < digits.length-1) {
-			digits[i] -= 10;
-			i++;
-			digits[i]++;
+	
+	// COMPARISONS
+	
+	// Equality
+	this.eq = function(n) {
+		if (this.negative != n.negative || this.digits.length != n.digits.length) {
+			return false;
 		}
-		if (digits[i-1] > 9 && i >= digits.length-1) {
-			digits[i-1] -= 10;
-			digits.push(1);
+		else {
+			for (var i=0,l=this.digits.length;i<l;i++) {
+				if (this.digits[i] != n.digits[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+	};
+	// Helper: Is the magnitude of m less than the magnitude of n?
+	function lmag(m,n) {
+		if (m.digits.length < n.digits.length) {
+			return true;
+		}
+		else if (m.digits.length > n.digits.length) {
+			return false;
+		}
+		else {
+			for (var i=m.digits.length-1;i>=0;i--) {
+				if (m.digits[i] < n.digits[i]) {
+					return true;
+				}
+				else if (m.digits[i] > n.digits[i]) {
+					return false;
+				}
+			}
+			return false;
 		}
 	}
+	// Helper: Is the magnitude of m greater than the magnitude of n?
+	function gmag(m,n) {
+		if (m.digits.length > n.digits.length) {
+			return true;
+		}
+		else if (m.digits.length < n.digits.length) {
+			return false;
+		}
+		else {
+			for (var i=m.digits.length-1;i>=0;i--) {
+				if (m.digits[i] > n.digits[i]) {
+					return true;
+				}
+				else if (m.digits[i] < n.digits[i]) {
+					return false;
+				}
+			}
+			return false;
+		}
+	}
+	// Less Than
+	this.lt = function(n) {
+		if (!this.negative && n.negative) {			// Positive ints are never less than negative ints
+			return false;
+		}
+		else if (this.negative && !n.negative) {	// Negative ints are always less than positive ints
+			return true;
+		}
+		else if (this.negative) {	// Both ints are negative: this < n iff this.mag > n.mag
+			return gmag(this,n);
+		}
+		else {						// Both ints are positive: this < n iff this.mag < n.mag
+			console.log('LMAG');
+			return lmag(this,n);
+		}
+	};
+	// Greater Than
+	this.gt = function(n) {
+		if (!this.negative && n.negative) {			// Positive ints are always greater than negative ints
+			return true;
+		}
+		else if (this.negative && !n.negative) {	// Negative ints are never greater than positive ints
+			return false;
+		}
+		else if (this.negative) {	// Both ints are negative: this > n iff this.mag < n.mag
+			return lmag(this,n);
+		}
+		else {						// Both ints are positive: this > n iff this.mag > n.mag
+			return gmag(this,n);
+		}
+	};
+	
+	// INCREMENT AND DECREMENT
+	
+	// Helper: Increment the absolute value
+	function _inc() {
+		var i = 0;
+		this.digits[i]++;
+		while (this.digits[i] > 9 && i < this.digits.length-1) {
+			this.digits[i] = 0;
+			i++;
+			this.digits[i]++;
+		}
+		if (this.digits[i-1] > 9 && i >= this.digits.length-1) {
+			this.digits[i-1] = 0;
+			this.digits.push(1);
+		}
+	}
+	// Helper: Decrement the absolute value
 	function _dec() {
 		var i = 0;
-		digits[i]--;
-		while (digits[i] < 0 && i < digits.length-1) {
-			digits[i] = 9;
+		this.digits[i]--;
+		while (this.digits[i] < 0 && i < this.digits.length-1) {
+			this.digits[i] = 9;
 			i++;
-			digits[i]--;
+			this.digits[i]--;
 		}
-		if (digits[digits.length-1] == 0 && digits.length > 1) {
-			digits.pop(digits.length-1);
+		if (this.digits[this.digits.length-1] == 0 && this.digits.length > 1) {
+			this.digits.pop(this.digits.length-1);
 		}
 	}
 	// Increment this bigint
 	this.inc = function() {
-		if (negative) {
+		if (this.negative) {
 			_dec();
 		} else {
 			_inc();
@@ -67,7 +161,26 @@ var BigInt = function() {
 	};
 	// Decrement this bigint
 	this.dec = function() {
-		_dec();
+		if (this.negative) {
+			_inc();
+		} else {
+			_dec();
+		}
 		return this;
+	};
+	
+	// ADDITION AND SUBTRACTION
+	
+	function _add(m,n) {
+		var sum = new BigInt(), sd = [], md = m.digits, nd = n.digits;
+		
+		
+		
+		return sum;
+	}
+	
+	// Addition
+	this.add = function(n) {
+		return _add(this,n);
 	};
 };
